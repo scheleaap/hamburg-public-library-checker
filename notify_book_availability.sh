42001ec2a5f9b35a96fd2ce3369a5ff789968990
scheleaap <git@wout.maaskant.info>
2022-09-06 11:54:55 +0200

    Set up Pyenv in shell script

--- notify_book_availability.sh
+++ notify_book_availability.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 readonly basedir="$(dirname "$0")"
 cd $basedir
 pipenv run ./notify_book_availability.py "$@"
